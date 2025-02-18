15d55098a2a459692197e5a57cf19b3b9b325d99
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-18 15:27:32 +0500

    Create script.js

---
+++ 02-loop-projects/script.js
@@ -0,0 +1,10 @@
+for (let i = 10; i <= 100; i+=10){
+    if(i == 50){
+        console.log("Half way there!");
+    }else if (i == 100) {
+        console.log("you made it!");
+    }else{
+        console.log("Checkpoint ", i);
+    }
+}
+console.log("All, Done");
